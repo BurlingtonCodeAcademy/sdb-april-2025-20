df5ddf8027ee4e911cf76446921c764d29fb8983
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-04 18:30:29 -0500

    multiple buttons with if else

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const box = document.getElementById('box');
 let myButton = document.getElementById('my-button')
 
+const prestoBtn = document.getElementById('presto');
+const abraBtn = document.getElementById('abra');
+const theRock = document.getElementById('the-rock');
+
 function alertMe() {
     alert("Button clicked!");
 }
@@ -10,3 +14,17 @@ box.addEventListener("click", () => {
 })
 
 myButton.addEventListener("click", alertMe)
+
+function sayMagicWord(event) {
+    if (event.target === prestoBtn) {
+        alert("Change-o!")
+    } else if (event.target === abraBtn) {
+        alert("Kadabra!")
+    } else {
+        alert("Shazam!");
+    }
+}
+
+prestoBtn.addEventListener("click", sayMagicWord);
+abraBtn.addEventListener("click", sayMagicWord);
+theRock.addEventListener("click", sayMagicWord);
